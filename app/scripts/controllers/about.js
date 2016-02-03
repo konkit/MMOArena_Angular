b99e77c620ddb5997abddc92595dbe7ab32cf09c
Łukasz Tenerowicz <konkit@gmail.com>
2016-02-03 20:32:34 +0100

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mmoarenaApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the mmoarenaApp
+ */
+angular.module('mmoarenaApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mmoarenaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the mmoarenaApp
+ */
+angular.module('mmoarenaApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
